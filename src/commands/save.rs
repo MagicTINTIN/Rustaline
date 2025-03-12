3cb9b31696aaab760ac6eb627ab4bdf138366fda
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-03-12 10:49:43 +0000

    fix: acknowledge interaction

--- src/commands/save.rs
+++ src/commands/save.rs
@@ -1,13 +1,28 @@
 use serenity::{
-    all::{CommandInteraction, Context, CreateMessage},
+    all::{CommandInteraction, Context, CreateInteractionResponse, CreateInteractionResponseMessage, CreateMessage},
     builder::CreateCommand,
 };
 
 pub async fn run(ctx: &Context, interaction: &CommandInteraction) {
-    let _ = interaction.user.direct_message(
-        ctx,
-        CreateMessage::new().content("Blabla"),
-    ).await;
+    let _ = interaction
+        .user
+        .direct_message(ctx, CreateMessage::new().content("Blabla"))
+        .await;
+    // let _ = interaction.create_response(
+    //     ctx,
+    //     serenity::all::CreateInteractionResponse::Message(
+    //         CreateInteractionResponseMessage::default().content("Saved !"),
+    //     ),
+    // );
+    let _test = interaction
+        .create_response(
+            &ctx,
+            CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::default()
+                    .content(format!("Saved !"))
+            ),
+        )
+        .await.unwrap();
     // interaction.defer_ephemeral(ctx);
 }
 
