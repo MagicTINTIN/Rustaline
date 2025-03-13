bf3b8e8ce4c136d5010ef5274d1211b26de1e4b9
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-03-13 12:48:33 +0000

    feat: save now sends the content of the message

--- src/commands/save.rs
+++ src/commands/save.rs
@@ -1,12 +1,37 @@
 use serenity::{
-    all::{CommandInteraction, Context, CreateInteractionResponse, CreateInteractionResponseMessage, CreateMessage},
+    all::{
+        CommandInteraction, Context, CreateInteractionResponse, CreateInteractionResponseMessage,
+        CreateMessage },
     builder::CreateCommand,
 };
 
+macro_rules! unwrap_or_return {
+    ( $e:expr ) => {
+        match $e {
+            Some(x) => x,
+            None => return,
+        }
+    };
+}
+
+fn truncate(s: &str, max_chars: usize) -> &str {
+    match s.char_indices().nth(max_chars) {
+        None => s,
+        Some((idx, _)) => &s[..idx],
+    }
+}
+
 pub async fn run(ctx: &Context, interaction: &CommandInteraction) {
+    // let msg = match interaction.data.resolved.messages.values().next() {
+    //     Some(msg) => msg.content.clone(),
+    //     None => "".to_owned()
+    // };
+    let msg = unwrap_or_return!(interaction.data.resolved.messages.values().next());
+    // let msg_to_send = format!("{}({} <{}>) : {}", msg.author.display_name(), msg.author.name, msg.author.email.clone().unwrap_or("unavailable email".to_string()), msg.content);
+    let msg_to_send = format!("{}({}) : {}", msg.author.display_name(), msg.author.name, msg.content);
     let _ = interaction
         .user
-        .direct_message(ctx, CreateMessage::new().content("Blabla"))
+        .direct_message(ctx, CreateMessage::new().content(truncate(&msg_to_send, 2000)))
         .await;
     // let _ = interaction.create_response(
     //     ctx,
@@ -18,11 +43,11 @@ pub async fn run(ctx: &Context, interaction: &CommandInteraction) {
         .create_response(
             &ctx,
             CreateInteractionResponse::Message(
-                CreateInteractionResponseMessage::default()
-                    .content(format!("Saved !"))
+                CreateInteractionResponseMessage::default().content(format!("Saved !")),
             ),
         )
-        .await.unwrap();
+        .await
+        .unwrap();
     // interaction.defer_ephemeral(ctx);
 }
 
